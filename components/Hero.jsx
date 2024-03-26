1d1f8a140ab54aae3664e6b76faed72407f58629
mc <dyd97wns@naver.com>
2024-03-26 23:19:25 +0900

    css change

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -39,9 +39,20 @@ const Hero = () => {
                 Download CV <Download size={18} />
               </Button>
             </div>
+            {/* socials */}
+            <Socials
+              containerStyles="flex gap-x-6 mx-auto xl:mx-0"
+              iconsStyles="text-foreground text-[22px] hover:text-primary transition-all"
+            />
           </div>
           {/* image */}
-          <div className="hidden xl:flex relative">image</div>
+          <div className="hidden xl:flex relative">
+            <div className="bg-hero_shape2_light dark:bg-hero_shape2_dark w-[500px] h-[500px] bg-no-repeat absolute -top-1 -right-2"></div>
+            <DevImg
+              containerStyles="bg-hero_shape w-[510px] h-[462px] bg-no-repeat realtive bg-bottom"
+              imgSrc="/hero/developer.png"
+            />
+          </div>
         </div>
         {/* icon */}
         <div className="hidden md:flex absolute left-2/4 bottom-44 xl:bottom-20 animate-bounce">
