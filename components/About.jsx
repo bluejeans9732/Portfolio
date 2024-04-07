44fab484fa0b2bfa12fb142c0e808042def239c8
mc <dyd97wns@naver.com>
2024-04-07 21:19:11 +0900

    About page Tabscontent end

--- components/About.jsx
+++ components/About.jsx
@@ -9,6 +9,7 @@ import {
   PhoneCall,
   Calendar,
   Briefcase,
+  GraduationCap,
 } from "lucide-react"
 
 const infoDate = [
@@ -39,8 +40,14 @@ const qualificationData = [
     title: "education",
     data: [
       {
+        university: "학원",
         qualification: "education",
         years: "2023.01 - 06",
+      },
+      {
+        university: "학원",
+        qualification: "education2",
+        years: "2023.01 - 062",
       },
     ],
   },
@@ -48,9 +55,14 @@ const qualificationData = [
     title: "experience",
     data: [
       {
-        company: "",
+        company: "not yet",
         qualification: "not yet",
-        years: "",
+        years: "not yet",
+      },
+      {
+        company: "not yet2",
+        qualification: "not yet2",
+        years: "not yet2",
       },
     ],
   },
@@ -87,7 +99,7 @@ const About = () => {
           </div>
           {/* tabs */}
           <div className="flex-1">
-            <Tabs bs defaultValue="personal">
+            <Tabs defaultValue="personal">
               <TabsList className="w-full grid xl:grid-cols-3 xl:max-w-[520px] xl:border dark:border-none">
                 <TabsTrigger className="w-[162px] xl:w-auto" value="personal">
                   Personal Info
@@ -126,9 +138,137 @@ const About = () => {
                   </div>
                 </TabsContent>
                 {/* quilfication */}
-                <TabsContent value="qualification">Qualification</TabsContent>
+                <TabsContent value="qualification">
+                  <div className="xl:text-left mx-auto">
+                    <h3 className="h3 mb-8 text-center xl:text-left">
+                      Qulification
+                    </h3>
+                    {/* Wrap */}
+                    <div className="grid md:grid-cols-2 gap-y-8">
+                      {/* experience */}
+                      <div className="flex flex-col gap-y-6">
+                        <div className="flex gap-x-4 items-center text-[22px] text-primary">
+                          <Briefcase />
+                          <h4 className="capitalize font-medium">
+                            {getData(qualificationData, "experience").title}
+                          </h4>
+                        </div>
+                        {/* list */}
+                        <div className="flex flex-col gap-y-8">
+                          {getData(qualificationData, "experience").data.map(
+                            (item, index) => {
+                              const { company, qualification, years } = item
+                              return (
+                                <div className="flex gap-x-8 group" key={index}>
+                                  <div className="h-[84px] w-[1px] bg-border relative ml-2">
+                                    <div className="w-[11px] h-[11px] rounded-full bg-primary absolute -left-[5px] group-hover:translate-y-[84px] transition-all duration-500"></div>
+                                  </div>
+                                  <div>
+                                    <div className="font-semibold text-xl leading-none mb-2">
+                                      {company}
+                                    </div>
+                                    <div className="text-lg leading-none text-muted-foreground mb-4">
+                                      {qualification}
+                                    </div>
+                                    <div className="text-base font-medium">
+                                      {years}
+                                    </div>
+                                  </div>
+                                </div>
+                              )
+                            }
+                          )}
+                        </div>
+                      </div>
+                      {/* education */}
+                      <div className="flex flex-col gap-y-6">
+                        <div className="flex gap-x-4 items-center text-[22px] text-primary">
+                          <GraduationCap size={28} />
+                          <h4 className="capitalize font-medium">
+                            {getData(qualificationData, "education").title}
+                          </h4>
+                        </div>
+                        {/* list */}
+                        <div className="flex flex-col gap-y-8">
+                          {getData(qualificationData, "education").data.map(
+                            (item, index) => {
+                              const { university, qualification, years } = item
+                              return (
+                                <div className="flex gap-x-8 group" key={index}>
+                                  <div className="h-[84px] w-[1px] bg-border relative ml-2">
+                                    <div className="w-[11px] h-[11px] rounded-full bg-primary absolute -left-[5px] group-hover:translate-y-[84px] transition-all duration-500"></div>
+                                  </div>
+                                  <div>
+                                    <div className="font-semibold text-xl leading-none mb-2">
+                                      {university}
+                                    </div>
+                                    <div className="text-lg leading-none text-muted-foreground mb-4">
+                                      {qualification}
+                                    </div>
+                                    <div className="text-base font-medium">
+                                      {years}
+                                    </div>
+                                  </div>
+                                </div>
+                              )
+                            }
+                          )}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </TabsContent>
                 {/* skills */}
-                <TabsContent value="skills">Skills</TabsContent>
+                <TabsContent value="skills">
+                  <div className="text-center xl:text-left">
+                    <h3 className="h3 mb-8">Skills & Tools</h3>
+                    {/* skills*/}
+                    <div className="mb-16">
+                      <h4 className="text-xl font-semibold">Skills</h4>
+                      <div className="border-b border-border mb-4"></div>
+                      {/* skills list */}
+                      <div>
+                        {getData(skillData, "skills").data.map(
+                          (item, index) => {
+                            const { name } = item
+                            return (
+                              <div
+                                className="w-2/4 text-center xl:text-left mx-auto xl:mx-0"
+                                key={index}
+                              >
+                                <div className="font-medium">{name}</div>
+                              </div>
+                            )
+                          }
+                        )}
+                      </div>
+                    </div>
+                    {/* tools */}
+                    <div>
+                      <h4 className="text-xl font-semibold mb-2 xl:text-left">
+                        Tools
+                      </h4>
+                      <div className="border-b border-border mb-4"></div>
+                      {/* tools list */}
+                      <div className="flex gap-x-8 justify-center xl:justify-start">
+                        {getData(skillData, "tools").data.map((item, index) => {
+                          const { imgPath } = item
+                          return (
+                            <div key={index}>
+                              <Image
+                                src={imgPath}
+                                width={48}
+                                height={48}
+                                alt=""
+                                priority
+                              />
+                            </div>
+                          )
+                        })}
+                      </div>
+                    </div>
+                  </div>
+                </TabsContent>
               </div>
             </Tabs>
           </div>
